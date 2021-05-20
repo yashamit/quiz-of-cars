c6b73e1eebb6aca53610b4b6d16d161bfbb5ee8f
yashamit <76246828+yashamit@users.noreply.github.com>
2021-05-20 16:59:08 +0530

    Add files via upload

---
+++ js/Code.js
@@ -0,0 +1,25 @@
+const accessCode1 = "AVENTADOR";
+const accessCode2 = "GTR-34";
+const accessCode3 = "SUPRA";
+
+function clues() {
+    
+    fill("white")
+    textSize(15)
+    text("V E T N D A R O A", 100,50)
+    fill("lightblue")
+    text("Hint: Known for shooting flames!!", 100,70)
+// add code for displaying rest of the names and the hints.
+fill("white")
+textSize(15)
+text("T R G 4 3 - ", 700,150)
+fill("lightblue")
+text("Hint: Known as the godzilla" ,  700 ,170)
+
+fill("white")
+textSize(15)
+text(" U R A P S ", 100,250)
+fill("lightblue")
+text("Hint: Toyota's sports car with a 2jz engine (fast and furious)" ,  100 ,270)
+
+}
